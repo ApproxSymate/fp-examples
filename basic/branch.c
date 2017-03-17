3a98a0efede66870fbb2e83b3bdac32255f124ff
Andrew Santosa <santosa_1999@yahoo.com>
2017-03-17 09:51:05 +0800

    Added basic/branch.c

---
+++ basic/branch.c
@@ -0,0 +1,28 @@
+/*
+ * Simple branching
+ * Copyright 2017 National University of Singapore
+ */
+
+#include <klee/klee.h>
+
+int main()
+{
+    int output;
+    int input_x, input_y;
+    char pred;
+
+    klee_track_error(&input_x, "input_x_error");
+    klee_track_error(&input_y, "input_y_error");
+
+    klee_make_symbolic(&pred, sizeof(char), "pred");
+
+    if (pred) {
+        output = input_x + input_y;
+    } else {
+        output = input_x * input_y;
+    }
+
+    klee_bound_error(output, 1.3);
+
+    return 0;
+}
