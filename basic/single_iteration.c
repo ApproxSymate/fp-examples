71324f4489be5ffa2116618626ab3b30f5a8b320
Andrew Santosa <dcsandr@nus.edu.sg>
2017-05-22 10:46:25 +0800

    Added basic/single_iteration.c

---
+++ basic/single_iteration.c
@@ -0,0 +1,25 @@
+/*
+ * Simple single-iteration loop example for numerical precision
+ * analysis. Loop breaking analysis (activated with -loop-breaking
+ * option) requires two iterations, but it has to do proper cleaning
+ * up when less than two iterations were executed.
+ *
+ * Copyright 2017 National University of Singapore
+ */
+
+#include <klee/klee.h>
+
+int main(int argc, char **argv) {
+  int c = 0;
+
+  klee_make_symbolic(&c, sizeof(c), "c");
+  klee_track_error(&c, "c_error");
+
+  for (int i = 0; i < 1; ++i) {
+    c++;
+  }
+
+  klee_bound_error(c, 1.3);
+
+  return 0;
+}
