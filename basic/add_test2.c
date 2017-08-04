c6add18c00238396771d8e9306aaa741576392f7
Andrew Santosa <dcsandr@nus.edu.sg>
2017-08-04 10:48:37 +0800

    Added basic/add_test.c, basic/add_test2.c and basic/array_test2.c

---
+++ basic/add_test2.c
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2017 National University of Singapore
+ *
+ * Author: Himeshi De Silva <himeshidslv@gmail.com>
+ */
+#include <klee/klee.h>
+#include <stdio.h>
+int add(int x, int y);
+int main() {
+  int a, b, answer;
+  klee_make_symbolic(&a, sizeof(a), "a");
+  klee_make_symbolic(&b, sizeof(b), "b");
+  klee_track_error(&a, "error_a");
+  klee_track_error(&b, "error_b");
+  answer = add(a, b);
+  klee_bound_error(answer, 1.3);
+  return 0;
+}
+int add(int x, int y) { return (x + y) * (x - y); }
---
+++ basic/array_test2.c
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2017 National University of Singapore
+ *
+ * Author: Himeshi De Silva <himeshidslv@gmail.com>
+ */
+#include <klee/klee.h>
+#include <stdio.h>
+
+void test_array(int *x);
+
+int arr[5], answer;
+
+int main() {
+
+  klee_make_symbolic(arr, sizeof arr, "arr");
+  klee_track_error(arr, "arr_error");
+
+  test_array(arr);
+
+  klee_bound_error(arr[0], 1.3);
+  return 0;
+}
+
+void test_array(int *x) {
+  int i;
+
+  // x[0] = (x[1] + x[2]) * (x[3] - x[4]);
+  x[0] = x[1] + x[2];
+
+  return;
+}
